d6453536091f567ef6550c6b959281176ca17023
ihlassovbetov <31760097+ihlassovbetov@users.noreply.github.com>
2017-09-09 23:29:29 +0300

    adding github url

--- qa-plugin.php
+++ qa-plugin.php
@@ -3,10 +3,11 @@
 /*
 	Plugin Name: FBinvite
 	Plugin Description: Invite your facebook friends to q2a.
+	Plugin URI: https://github.com/ihlassovbetov/q2a-fbinviter
 	Plugin Version: 1.0
 	Plugin Date: 05/09/2017
 	Plugin Author: Yhlas Sovbetov
-	Plugin Author URI: E-Dostluk Group www.e-dostluk.com / gyzgyn.e-dostluk.com
+	Plugin Author URI: https://github.com/ihlassovbetov
 	Plugin License: GPLv3
 	Plugin Tested on: Q2A 1.7.4
 	
@@ -27,11 +28,11 @@
 			header('Location: ../../');
 			exit;
 	}
-	qa_register_plugin_layer('qa-fbinvite-layer.php', 'FBinvite Layer');	
+	qa_register_plugin_layer('qa-fbinvite-layer.php', 'FBinviter Layer');	
 		
-	qa_register_plugin_module('module', 'qa-fbinvite-admin.php', 'qa_fbinvite_admin', 'FBinvite Admin');
+	qa_register_plugin_module('module', 'qa-fbinvite-admin.php', 'qa_fbinvite_admin', 'FBinviter Admin');
 
-	qa_register_plugin_module('widget', 'qa-fbinvite-widget.php', 'qa_fbinvite_widget', 'FBinvite Widget');
+	qa_register_plugin_module('widget', 'qa-fbinvite-widget.php', 'qa_fbinvite_widget', 'FBinviter Widget');
 
 /*
 	Omit PHP closing tag to help avoid accidental output
